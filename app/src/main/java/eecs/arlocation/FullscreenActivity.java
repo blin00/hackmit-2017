9af0016a0b22545f459f9ac28fc88660329a400e
Brandon Lin <blin00@gmail.com>
2017-09-17 03:33:25 -0400

    fix main thread start

--- app/src/main/java/eecs/arlocation/FullscreenActivity.java
+++ app/src/main/java/eecs/arlocation/FullscreenActivity.java
@@ -370,6 +370,9 @@ public class FullscreenActivity extends AppCompatActivity implements SensorEvent
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+        if (locationController.checkPermission()) {
+            updateHandler.post(updateRunnable);
+        }
     }
 
     private void startPreview() {
@@ -423,7 +426,6 @@ public class FullscreenActivity extends AppCompatActivity implements SensorEvent
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("AR", "camera perms success");
                 finishSetupCamera();
-                locationController.checkPermission();
             } else {
                 // try again
                 Log.d("AR", "camera perms retry");
