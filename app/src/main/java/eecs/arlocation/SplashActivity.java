d8ea5d694f5ebfff937966492470f70b3c6445fa
Alex Chen <username@mit.edu>
2017-09-16 21:51:08 -0400

    temp

--- app/src/main/java/eecs/arlocation/SplashActivity.java
+++ app/src/main/java/eecs/arlocation/SplashActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by chennosaurus on 9/16/17.
@@ -19,21 +21,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        mContentView = findViewById(R.id.splash);
-
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        setContentView(R.layout.splash);
+//set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_splash);
 
 
 
